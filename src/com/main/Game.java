73935c52d34c84653b9d3bd2c3afa5b276b76372
VinAlvini <alvinclnt@gmail.com>
2020-08-18 09:47:15 +0300

    First commit

---
+++ src/com/main/Game.java
@@ -0,0 +1,20 @@
+package com.main;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable{
+	
+	public Game() {
+		
+	}
+	public void start() {
+		//game start
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

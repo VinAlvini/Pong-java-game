498f10c9598749559eea8e5793bd0a12221df8dd
Alvin Antony <alvinclnt@gmail.com>
2020-08-18 20:34:57 +0300

    second commit
    set up of windows

--- src/com/main/Game.java
+++ src/com/main/Game.java
@@ -1,10 +1,21 @@
 package com.main;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
 
 public class Game extends Canvas implements Runnable{
 	
+	public static final int WIDTH =1000;
+	public static final int HEIGHT =WIDTH * 9/16;
 	public Game() {
+		canvasSetup();
+		new Window("Pong", this);
+		
+	}
+	private void canvasSetup() {
+		this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		this.setMaximumSize(new Dimension(WIDTH,HEIGHT));
+		this.setMinimumSize(new Dimension(WIDTH,HEIGHT));
 		
 	}
 	public void start() {
@@ -13,7 +24,11 @@ public class Game extends Canvas implements Runnable{
 	}
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
+		
+	}
+	public static void main(String[] args) {
+		new Game();
 		
 	}
 
--- src/com/main/Window.java
+++ src/com/main/Window.java
@@ -1,5 +1,19 @@
 package com.main;
 
+import javax.swing.JFrame;
+
 public class Window {
+	Window(String title, Game game){
+		JFrame jFrame = new JFrame(title);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setResizable(false);
+		jFrame.add(game);
+		jFrame.pack();
+		jFrame.setLocationRelativeTo(null);
+		jFrame.setVisible(true);
+		
+		game.start();
+		
+	}
 
 }
